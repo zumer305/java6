c84da8bf15660a74086c1e128febc4063aa9bbfd
zumer305 <zumerniaz305@gmail.com>
2025-09-16 03:22:47 +0700

    update

---
+++ app.js
@@ -0,0 +1,106 @@
+// printing by function
+function hello(){
+    console.log("My name is zumer");
+    console.log("I am a girl");
+}
+hello();
+
+
+// loop by function
+function hello2(){
+    
+    for(let i=1;i<=5;i++){
+    console.log(i);
+    }
+}
+hello2();
+
+
+// loop by function
+function hello3(){
+    let age=14;
+    
+   if(age<18) console.log("not adult");
+   else console.log("adult");
+}
+hello3();
+
+
+// practice qs
+function poem()
+{
+    console.log("Twinke Twinkle Little star. HOW i WONDER what you ARE");
+}
+poem();
+
+
+// roll dice
+function roller(){
+    let rand=Math.floor(Math.random()*6)+1;
+    console.log(rand);
+}
+roller();
+roller();
+roller();
+
+
+// arguments
+function info(name,age){
+    console.log(`${name}'s age is ${age}`);
+}
+info("zumer",23);
+info("alishba",24);
+info("wahab",20);
+info("mama",44);
+
+
+// sum
+function sum(a,b){
+    console.log(a+b);
+}
+sum(40,40);
+
+
+// avg
+function avg(a,b){
+    console.log((a+b)/2);
+}
+avg(40,40);
+
+
+
+// table
+function table(num){
+    for(let i=0;i<=num*10;i=i+num){
+    console.log(i);
+    }
+}
+table(2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
